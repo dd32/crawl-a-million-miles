7c995b6ccf3b11288dc7f8d7f2240309b61a9ac0
Dion Hulse <contact@dd32.id.au>
2022-05-25 18:05:21 +1000

    Add some Stats and WP detection.

--- crawl.php
+++ crawl.php
@@ -16,6 +16,39 @@ const TIMEOUT     = 5.0;
 const MAX_DOMAINS = 100; // Maximum number to process, set to 0 for all.
 const USER_AGENT  = 'dd32-CrawlaMillion/1.0; https://github.com/dd32/crawl-a-million-miles';
 
+$stats = [
+	'success' => 0,
+	'error'   => 0,
+	'wp'      => [
+		'yes'   => 0,
+		'maybe' => 0,
+		'no'    => 0,
+	]
+];
+
+function stat( $stat, $sub = null ) {
+	global $stats;
+	if ( ! isset( $stats[ $stat ] ) ) {
+		$stats[ $stat ] = 0;
+	}
+
+	if ( is_null( $sub ) ) {
+		if ( is_int( $stats[ $stat ] ) ) {
+			$stats[ $stat ]++;
+		}
+	} else {
+		if ( ! is_array( $stats[ $stat ] ) ) {
+			$stats[ $stat ] = [
+				$sub => 0
+			];
+		} elseif ( ! isset( $stats[ $stat ][ $sub ] ) ) {
+			$stats[ $stat ][ $sub ] = 0;
+		}
+
+		$stats[ $stat ][ $sub ]++;
+	}
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
 /**
@@ -45,14 +78,54 @@ function gen_domains() {
  * The callback for when we have the HMTL content of a domain.
  */
 function callback_success( $domain, $response ) {
-	echo "$domain returned HTTP " . $response->getStatusCode() . ' and ' . strlen( $response->getBody() ) . " bytes \n";
+	$code = $response->getStatusCode();
+
+	stat( 'success' );
+	stat( 'code', $code );
+
+	$body         = $response->getBody();
+	$headers      = $response->getHeaders();
+	$link_headers = implode( ' ', $headers['Link'] ?? [] );
+
+	if (
+		str_contains( $link_headers, 'https://api.w.org/' ) ||
+		str_contains( $link_headers, '/wp-json' ) ||
+		str_contains( $link_headers, '?rest_route=' ) ||
+		str_contains( $body, '/wp-' ) ||
+		str_contains( $body, 'WordPress/' ) ||
+		str_contains( $body, '/xmlrpc.php">' )
+	) {
+		stat( 'wp', 'yes' );
+	} elseif (
+		// Probably..
+		str_contains( $body, '?rest_route=' ) ||
+		str_contains( $body, '.wp.com/' )
+	) {
+		stat( 'wp', 'maybe' );
+	} else {
+		stat( 'wp', 'no' );
+	}
+
+	echo "$domain returned HTTP " . $code . ' and ' . strlen( $response->getBody() ) . " bytes \n";
 }
 
 /**
  * The callback for when we timeout on a domain.
  */
-function callback_failure( $domain, $exception ) {
-	echo "$domain threw an error: " . $exception->getMessage() . "\n";
+function callback_failure( $domain, Exception $e ) {
+	stat('error');
+
+	$message = $e->getMessage();
+	$code    = '';
+	if ( preg_match( '!\(([^)]+)\)[^)]*$!', $message, $m ) ) {
+		$code = $m[1];
+	}
+
+	if ( $code ) {
+		stat( 'error-reason', $code );
+	}
+
+	echo "$domain threw an error: " . $message . "\n";
 }
 
 $browser = new Browser(
@@ -88,7 +161,7 @@ $que = new Queue(
 );
 
 // Fill the Queue up every now and then.
-$timer = Loop::addPeriodicTimer( 1.0, function () use( &$timer, $que ) {
+Loop::addPeriodicTimer( 1.0, function( $timer ) use( $que ) {
 	// Setup the domains..
 	static $domains = false;
 	if ( ! $domains ) {
@@ -119,3 +192,21 @@ $timer = Loop::addPeriodicTimer( 1.0, function () use( &$timer, $que ) {
 	}
 } );
 
+Loop::addSignal( SIGINT, function( int $signal ) {
+    echo "\nCaught user interrupt signal\n";
+	Loop::stop();
+} );
+
+Loop::run();
+
+print_r( $stats );
+
+printf(
+	"Processed %d sites, success rate of %s\n",
+	$stats['success'] + $stats['error'],
+	round( ( $stats['success'] / ( $stats['success'] + $stats['error'] ) ) * 100, 1 ) . '%'
+);
+printf(
+	"WordPress was seen on %s of successful sites.\n",
+	round( ( $stats['wp']['yes'] + $stats['wp']['maybe'] ) / $stats['success'] * 100, 2 ) . '%'
+);
